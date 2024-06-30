eb29696eb5e652c66f1c247442ed7a80893954db
bluescreenlife <bluescreenlife91@gmail.com>
2024-06-30 13:30:48 -0500

    redesign, add delete button

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from "react";
 import ReactCardFlip from "react-card-flip";
+import { RiFlipVerticalFill } from "react-icons/ri";
+import { IoTrashBinOutline } from "react-icons/io5";
 
 const Card = ({ data }) => {
   // useState and function for flipping cards on tap/click
@@ -20,38 +22,41 @@ const Card = ({ data }) => {
     >
       {/* card front */}
       <div
-        className="card-front p-4 h-36 text-stone-100 bg-gradient-to-b from-blue-800 to-purple-600 rounded-lg  "
+        className="card-front flex flex-col divide-y-2 divide-dotted divide-purple-500 h-40 text-stone-100 bg-gradient-to-b from-blue-800 to-purple-600 rounded-lg  "
         onClick={flipCard}
       >
-        <span className="concert-info">
+        <span className="concert-info h-2/3 my-4 px-4 font-DMsans">
           <span className="font-bold text-lg">{data.artist}</span>
           <span className="block text-md">{data.date}</span>
-          <span className="block text-sm text-nowrap">
+          <span className="block text-sm italic text-nowrap">
             {data.venue}, {data.city}{" "}
           </span>
         </span>
-        <p className="flip-notice text-sm text-right mr-2 pt-4 text-purple-400">
-          tap to flip
-        </p>
       </div>
       {/* card back */}
-      <div
-        className="card-back p-4 h-full md:h-36 text-stone-100 bg-gradient-to-b from-blue-600 to-teal-600 rounded-lg"
-        onClick={flipCard}
-      >
-        <div className="setlist-container h-full">
-          <span className="setlist-title font-bold sm:text-sm md:h-1/5 md:text-base">
-            {data.artist}: Setlist
-          </span>
-          <span className="setlist-text block text-sm md:h-4/5 md:overflow-y-scroll">
-            {data.setlist
-              ? data.setlist.map((song, index) => (
-                  <span key={index} className="block">
-                    {index + 1}. {song}
-                  </span>
-                ))
-              : "Setlist unavailable."}
-          </span>
+      <div className="card-back flex flex-row cols-2 text-stone-100 bg-gradient-to-b from-blue-600 to-teal-600 rounded-lg">
+        <div className="flex-parent w-4/5">
+          <button className="flex w-full" onClick={flipCard}>
+            <div className="setlist-container h-full md:h-40 flex-grow text-left p-4 font-DMsans">
+              <span className="setlist-title font-bold sm:text-sm md:h-1/5 md:text-base">
+                {data.artist}: Setlist
+              </span>
+              <span className="setlist-text block text-sm md:h-4/5 md:overflow-y-scroll">
+                {data.setlist
+                  ? data.setlist.map((song, index) => (
+                      <span key={index} className="block">
+                        {index + 1}. {song}
+                      </span>
+                    ))
+                  : "Setlist unavailable."}
+              </span>
+            </div>
+          </button>
+        </div>
+        <div className="button-container flex w-1/5">
+          <button className="p-6">
+            <IoTrashBinOutline />
+          </button>
         </div>
       </div>
     </ReactCardFlip>
